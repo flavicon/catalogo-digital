eb80b249b7ff67f6bab3b3fa90ae40288911e598
flaviojasilva <flaviojrs.pf@gmail.com>
2021-04-12 13:50:54 -0300

    Criação de cards para dashboard

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -1,6 +1,57 @@
 function dashboard() {
   return `
     ${navbar()}
-    <h1>Dashboard</h1>
+    <div class="row justify-content-md-center">
+      <div class="col-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm">
+                <span class="material-icons" style="font-size: 8rem;">
+                  store
+                </span>
+              </div>
+              <div class="col-sm text-center mt-3">
+                <p class="h3">Lojas</p>
+                <p class="h1">30</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm">
+                <span class="material-icons" style="font-size: 8rem;">
+                  pin_drop
+                </span>
+              </div>
+              <div class="col-sm text-center mt-3">
+                <p class="h3">Regiões</p>
+                <p class="h1">10</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+          <div class="card-body">
+            <div>
+              <div>
+                <span class="material-icons" style="font-size: 6rem;">
+                  library_add
+                </span>
+              </div>
+              <div>
+                <p class="h4">Gerenciar catálogo</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   `
 }
